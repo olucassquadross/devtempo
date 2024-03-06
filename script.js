a7ef22cf896f3ba58f4b59b290e9da6e72d745df
Lucas Quadros <olucasquadros@gmail.com>
2024-03-06 20:15:27 -0300

    api previssão do tempo

---
+++ script.js
@@ -0,0 +1,5 @@
+function getWeather() {
+    const apiKey = 'd7a8d944e376f6e9e6fd3af2bb39f6d9';
+
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+}
