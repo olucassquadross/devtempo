9888d2c1e835555eac027fc229b7fe5b63f9c228
Lucas Quadros <olucasquadros@gmail.com>
2024-03-06 21:29:49 -0300

    change city

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getWeather() {
     const apiKey = 'd7a8d944e376f6e9e6fd3af2bb39f6d9';
-
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+    const apiCity = document.getElementById('cidade').value;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=${apiKey}&units=metric`;
 
     fetch(apiURL)
     .then(response => response.json())
@@ -21,4 +21,4 @@ function getWeather() {
 
 }
 
-getWeather();
+document.getElementById('cidade').addEventListener('input', getWeather);
